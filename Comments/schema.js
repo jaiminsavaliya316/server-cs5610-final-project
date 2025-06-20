394c357969a8bfb35817eb116f43228a8803664b
William Seward <wdseward03@gmail.com>
2025-06-19 20:46:40 -0400

    comments schema model dao routes

---
+++ Comments/schema.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+const commentSchema = new mongoose.Schema(
+  {
+    movieId: String,
+    author: String,
+    text: String,
+    timestamp: Date,
+  },
+  { collection: 'comments' }
+);
